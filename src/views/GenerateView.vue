5e5147e182dc6c17e6c86417464909b0640775a1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-30 18:06:43 +0900

    とりあえずダミーのMarkdownを表示してみた

--- src/views/GenerateView.vue
+++ src/views/GenerateView.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRoute } from "vue-router";
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
 
 const route = useRoute();
 const operation = ref(route.query.operation);
 const innovation = ref(route.query.innovation);
+const response = ref("# Test\n##Test");
 
 defineExpose({
+  MdEditor,
   operation,
   innovation,
+  response,
 });
 </script>
 
 <template lang="pug">
 .generateView
-  div {{ operation }}に対して{{ innovation }}を組み合わせる
+  MdEditor(v-model="response" :preview-only="true")
 </template>
 
 <style lang="sass" scoped>
