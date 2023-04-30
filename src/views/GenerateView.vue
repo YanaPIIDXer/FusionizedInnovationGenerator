15f9048d0450dd312ba0905417e707712a735260
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-30 18:29:25 +0900

    とりあえず試しに適当なプロンプトを投げてみた

--- src/views/GenerateView.vue
+++ src/views/GenerateView.vue
@@ -1,26 +1,47 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
-import MdEditor from 'md-editor-v3';
-import 'md-editor-v3/lib/style.css';
+import { ChatGPT } from "../chatgpt";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+import ProgressSpinner from "primevue/progressspinner";
 
 const route = useRoute();
 const operation = ref(route.query.operation);
 const innovation = ref(route.query.innovation);
-const response = ref("# Test\n##Test");
+const response = ref("");
+const chatGpt = new ChatGPT();
+const isProgress = ref(false);
+
+onMounted(async () => {
+  isProgress.value = true;
+  try {
+    response.value = await chatGpt.sendMessage("TypeScriptでHello Worldを書いてください", false);
+  } catch (error) {
+  } finally {
+    isProgress.value = false;
+  }
+});
 
 defineExpose({
   MdEditor,
   operation,
   innovation,
   response,
+  ProgressSpinner,
+  isProgress,
 });
 </script>
 
 <template lang="pug">
 .generateView
   MdEditor(v-model="response" :preview-only="true")
+  ProgressSpinner.spinner(v-if="isProgress")
 </template>
 
 <style lang="sass" scoped>
+.generateView
+  .spinner
+    width: 25%
+    height: 25%
 </style>
