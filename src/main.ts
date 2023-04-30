ca5385153ab5836cc1930f57d3ee53e20ab9ddef
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-30 17:16:31 +0900

    PrimeVue導入

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
+
+// PrimeVue
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+app.use(PrimeVue);
+
 app.mount("#app");
